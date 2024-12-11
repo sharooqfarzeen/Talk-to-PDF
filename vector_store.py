59dbf04fee148bde5afafc0115947dfe0c424d42
sharooqfarzeen <sharooqfarzeen@gmail.com>
2024-12-11 09:31:01 +0530

    Fixed session_state issue

--- vector_store.py
+++ vector_store.py
@@ -3,9 +3,8 @@ from langchain_community.vectorstores import FAISS
 
 import streamlit as st
 
-api_key = st.session_state.api_keys["GOOGLE_API_KEY"]
 
-def create_vector_store(chunks, embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)):
+def create_vector_store(chunks, embeddings):
     # Creates the vector store
     vector_store = FAISS.from_texts(texts=chunks, embedding=embeddings)
     # Stores it locally
